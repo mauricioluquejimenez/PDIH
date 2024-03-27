ecf46033674a9080af042e8c6ea3210b21fa34a5
Mauricio Luque Jiménez <72136756+mauricioluquejimenez@users.noreply.github.com>
2024-03-27 18:25:35 +0100

    Documentación para funciones implementadas

--- S2/teclado.c
+++ S2/teclado.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <dos.h>
 
+/*
+* @brief Leer carácter por pantalla
+* return outregs.h.al: registro que contiene el carácter leído por teclado
+*/
 int getchar_S2(){
   union REGS inregs, outregs;
 
@@ -10,6 +14,10 @@ int getchar_S2(){
   return outregs.h.al;
 }
 
+/*
+* @brief Escribir un carácter en pantalla
+* @param c: carácter a mostrar por pantalla
+*/
 void putchar_S2(char c){
   union REGS inregs, outregs;
 
